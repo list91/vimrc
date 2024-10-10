679fc813ddb851d9e067c5a80d241e9a00d5003d
stanislav918 <stanislav@fulldive.com>
2024-10-11 00:18:09 +0700

    test2

--- pluginstall.sh
+++ pluginstall.sh
@@ -1,2 +1,2 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubasdlkdposgkopfdkgopkdfopkpodfgopm
+kjdfgjdfnkgnfdjgjkfdngkjfdlnjfdljbnbjlnbjlnlf
+fdbldkfbnljdfnbldbnlkfklbf
